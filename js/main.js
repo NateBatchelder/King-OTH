35f082805b8bcbde819f5e397463be7c9556d531
Nathan Batchelder <natebatchelder@gmail.com>
2022-01-06 13:09:09 -0800

    added styling rules for rank user interface

---
+++ js/main.js
@@ -0,0 +1,21 @@
+import Game from "./rank.js";
+import GameView from "./rankUI.js";
+
+let game = new Game();
+let gameView = new GameView(document.getElementById("app"));
+
+// define functions
+gameView.onTileClick = function (i) {
+  game.makeMove(i);
+  gameView.update(game);
+  console.log(`Array tile ${i} clicked.`);
+};
+
+gameView.onRestartClick = function () {
+  game = new Game();
+  gameView.update(game);
+  console.log("Restart button clicked, game is restarted!");
+};
+
+// update the gameView with the game data
+gameView.update(game);
