57cb1e1268a30b33d4d400c22e8b5d2b431a3d6e
Carter Supple <Cartersupple123@gmail.com>
2022-01-07 15:24:46 -0800

    require paths to fix partials error

--- public/app.js
+++ public/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000
+const path = require('path')
 
 const { engine } = require('express-handlebars');
+
 
 app.set('view engine', 'hbs');
 
